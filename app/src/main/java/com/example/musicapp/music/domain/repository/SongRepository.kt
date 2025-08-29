0fbf9cc9c0e1e4ab652da1846e6fb49614f1e9f6
dinh-phuc <dinhphucaz52@gmail.com>
2025-08-29 22:11:18 +0700

    refactor: deprecate unused methods in SongRepository interface

--- app/src/main/java/com/example/musicapp/music/domain/repository/SongRepository.kt
+++ app/src/main/java/com/example/musicapp/music/domain/repository/SongRepository.kt
@@ -9,30 +9,46 @@ import kotlinx.coroutines.flow.Flow
 
 interface SongRepository {
 
+    @Deprecated("No longer used")
     suspend fun getSongsFromPlaylist(playlistId: Int): List<Song>
 
+    @Deprecated("No longer used")
     suspend fun createPlayList(name: String)
 
+    @Deprecated("No longer used")
     suspend fun updatePlaylist(playlistId: Int, name: String)
 
+    @Deprecated("No longer used")
     suspend fun deletePlayList(id: Int)
 
+    @Deprecated("No longer used")
     suspend fun addSongsToPlaylist(playListId: Int, localSongs: List<Song>)
 
+    @Deprecated("No longer used")
     suspend fun deleteSongs(songIds: List<String>)
 
+    @Deprecated("No longer used")
     suspend fun getLocalSong(): List<LocalSong>
 
     fun getPlayLists(): Flow<List<Playlist>>
 
+    @Deprecated("No longer used")
     suspend fun likeSong(song: Song)
 
+    @Deprecated("No longer used")
     suspend fun unlikeSong(song: Song)
 
     fun getLikedSongs(): Flow<List<Song>>
 
-    suspend fun savePlaylist(name: String, description: String, createdBy: String, songs: List<SongEntity>)
+    @Deprecated("No longer used")
+    suspend fun savePlaylist(
+        name: String,
+        description: String,
+        createdBy: String,
+        songs: List<SongEntity>
+    )
 
+    @Deprecated("No longer used")
     suspend fun getPlaylists(): List<PlaylistEntity>
 
 }
